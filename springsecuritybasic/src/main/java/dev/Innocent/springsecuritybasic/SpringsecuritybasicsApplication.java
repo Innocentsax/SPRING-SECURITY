7cb844c9c63c569b68d9eea6008e94f072144f9a
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-12-29 14:10:33 +0100

    Spring Security Basic

---
+++ springsecuritybasic/src/main/java/dev/Innocent/springsecuritybasic/SpringsecuritybasicsApplication.java
@@ -0,0 +1,13 @@
+package dev.Innocent.springsecuritybasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritybasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuritybasicsApplication.class, args);
+	}
+
+}
---
+++ springsecuritybasic/src/test/java/dev/Innocent/springsecuritybasic/SpringsecuritybasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Innocent.springsecuritybasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritybasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
