b6dc0fb83f56681435b79aaa7941f3daf70b3dbd
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-12-30 16:22:14 +0100

    Defining and managing users

---
+++ springsecsection3/src/main/java/dev/Innocent/Model/Customer.java
@@ -0,0 +1,49 @@
+package dev.Innocent.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String email;
+    private String pwd;
+    private String role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
--- springsecsection3/src/main/java/dev/Innocent/UdoBankBackendApplication.java
+++ springsecsection3/src/main/java/dev/Innocent/UdoBankBackendApplication.java
@@ -2,10 +2,16 @@ package dev.Innocent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @ComponentScan("dev.Innocent.controller") // Optional
+@EnableJpaRepositories("dev.Innocent.repository") // Optional
+@EntityScan("dev.Innocent.model") // Optional
+@EnableWebSecurity // Optional
 public class UdoBankBackendApplication {
 
 	public static void main(String[] args) {
