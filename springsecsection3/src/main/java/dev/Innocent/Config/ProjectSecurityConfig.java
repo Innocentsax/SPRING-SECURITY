c08a6f1551e35542e53a447fbaf0d54f8dc62fa6
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-12-30 16:49:28 +0100

    Defining and managing users

--- springsecsection3/src/main/java/dev/Innocent/Config/ProjectSecurityConfig.java
+++ springsecsection3/src/main/java/dev/Innocent/Config/ProjectSecurityConfig.java
@@ -4,13 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -46,10 +42,10 @@ public class ProjectSecurityConfig {
 //    }
 
 
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource){
-        return new JdbcUserDetailsManager(dataSource);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource){
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
 
     /*
      *  NoOpPasswordEncoder is not recommended for production usage, Use only for non-prod
