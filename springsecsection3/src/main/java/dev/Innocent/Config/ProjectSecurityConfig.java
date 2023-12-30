788372cb6e54e84014dfc3e432069b8f9b5aa838
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-12-30 12:26:04 +0100

    Defining and managing users

--- springsecsection3/src/main/java/dev/Innocent/Config/ProjectSecurityConfig.java
+++ springsecsection3/src/main/java/dev/Innocent/Config/ProjectSecurityConfig.java
@@ -4,22 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectSecurityConfig {
-
-    // Authenticate all methods
-    @Bean
-    SecurityFilterChain defaultSecurityFilterChainDefault(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> {
-            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
-        });
-        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
-        return (SecurityFilterChain)http.build();
-    }
 
     // Below is the custom security configuration
     @Bean
@@ -32,22 +23,14 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
-
-    // Configuration to deny all the requests
     @Bean
-    SecurityFilterChain defaultSecurityFilterChainDenyAll(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
-        return http.build();
-    }
+    public InMemoryUserDetailsManager userDetailsService(){
+        /* Approach 1: where we use withDefaultPasswordEncoder() method while creating the user details */
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin").password("12345").authorities("admin").build();
 
-    // Configuration to permit all the requests
-    @Bean
-    SecurityFilterChain defaultSecurityFilterChainPermitAll(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
-        return http.build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user").password("12345").authorities("read").build();
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
