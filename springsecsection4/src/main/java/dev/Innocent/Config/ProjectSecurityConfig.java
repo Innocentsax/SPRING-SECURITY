390cb53d7a576cdcbb9f7a41770de0ea2e6cc2ff
Innocent Udo <innocentcharlesudo@gmail.com>
2023-12-31 14:20:33 +0100

    Password Management with Password Encoder (Bycrpt)

--- springsecsection4/src/main/java/dev/Innocent/Config/ProjectSecurityConfig.java
+++ springsecsection4/src/main/java/dev/Innocent/Config/ProjectSecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -23,39 +23,9 @@ public class ProjectSecurityConfig {
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
-
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsService(){
-//        /* Approach 1: where we use withDefaultPasswordEncoder() method while creating the user details */
-//        UserDetails admin = User.withDefaultPasswordEncoder()
-//                .username("admin").password("12345").authorities("admin").build();
-//
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("user").password("12345").authorities("read").build();
-//        return new InMemoryUserDetailsManager(admin, user);
-
-        // Approach 2 where we use NoOpPasswordEncoder Bean while creating the user details
-//        UserDetails admin = User.withUsername("admin").password("12345")
-//                .authorities("admin").build();
-//
-//        UserDetails user = User.withUsername("user").password("12345")
-//                .authorities("read").build();
-//        return new InMemoryUserDetailsManager(admin, user);
-//    }
-
-
-//    @Bean
-//    public UserDetailsService userDetailsService(DataSource dataSource){
-//        return new JdbcUserDetailsManager(dataSource);
-//    }
-
-    /*
-     *  NoOpPasswordEncoder is not recommended for production usage, Use only for non-prod
-     * @return  PasswordEncoder
-     */
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
 }
--- springsecsection4/src/main/java/dev/Innocent/Contoller/LoginController.java
+++ springsecsection4/src/main/java/dev/Innocent/Contoller/LoginController.java
@@ -5,6 +5,7 @@ import dev.Innocent.Repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
     CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public LoginController(CustomerRepository customerRepository) {
+    public LoginController(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
         this.customerRepository = customerRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/register")
@@ -23,6 +26,8 @@ public class LoginController {
         Customer savedCustomer = null;
         ResponseEntity response = null;
         try{
+            String hashPwd = passwordEncoder.encode(customer.getPwd());
+            customer.setPwd(hashPwd);
             savedCustomer = customerRepository.save(customer);
             if(savedCustomer.getId()>0){
                 response = ResponseEntity.status(HttpStatus.CREATED)
