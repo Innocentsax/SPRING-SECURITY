2d64a10adba715eade4e73d06770252ec5e32b01
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2024-01-01 12:10:12 +0100

    CORS & CSRF

--- Section6/springsecsection6/src/main/java/dev/Innocent/Config/ProjectSecurityConfig.java
+++ Section6/springsecsection6/src/main/java/dev/Innocent/Config/ProjectSecurityConfig.java
@@ -17,7 +17,7 @@ public class ProjectSecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+                        .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards","/user").authenticated()
                         .requestMatchers("/notices","/contact","/register").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
