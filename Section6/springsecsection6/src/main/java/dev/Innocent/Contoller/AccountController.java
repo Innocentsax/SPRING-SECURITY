95175ffd20c6375e3d99d2764d717632a93eb596
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2024-01-01 09:58:53 +0100

    CORS & CSRF

--- Section6/springsecsection6/src/main/java/dev/Innocent/Contoller/AccountController.java
+++ Section6/springsecsection6/src/main/java/dev/Innocent/Contoller/AccountController.java
@@ -1,13 +1,26 @@
 package dev.Innocent.Contoller;
 
+import dev.Innocent.Model.Accounts;
+import dev.Innocent.Repository.AccountsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
 
+    @Autowired
+    private AccountsRepository accountsRepository;
+
     @GetMapping("/myAccount")
-    public String getAccountsDetails(){
-        return "Here are the account details from my DB";
+    public Accounts getAccountDetails(@RequestParam int id) {
+        Accounts accounts = accountsRepository.findByCustomerId(id);
+        if (accounts != null ) {
+            return accounts;
+        }else {
+            return null;
+        }
     }
+
 }
