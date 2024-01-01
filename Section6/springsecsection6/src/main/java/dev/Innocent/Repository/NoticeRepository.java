d78c8acbdedbf871a30e6f359803901304bd89d0
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2024-01-01 09:54:31 +0100

    CORS & CSRF

---
+++ Section6/springsecsection6/src/main/java/dev/Innocent/Repository/NoticeRepository.java
@@ -0,0 +1,16 @@
+package dev.Innocent.Repository;
+
+import dev.Innocent.Model.Notice;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NoticeRepository extends CrudRepository<Notice, Long> {
+
+    @Query(value = "from Notice n where CURDATE() BETWEEN noticBegDt AND noticEndDt")
+    List<Notice> findAllActiveNotices();
+
+}
