765d5a9e2630cf657ad4e0264c3e8259938e7119
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2024-01-01 10:01:53 +0100

    CORS & CSRF

--- Section6/springsecsection6/src/main/java/dev/Innocent/Contoller/CardsController.java
+++ Section6/springsecsection6/src/main/java/dev/Innocent/Contoller/CardsController.java
@@ -1,13 +1,28 @@
 package dev.Innocent.Contoller;
 
+import dev.Innocent.Model.Cards;
+import dev.Innocent.Repository.CardsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CardsController {
 
+    @Autowired
+    private CardsRepository cardsRepository;
+
     @GetMapping("/myCards")
-    public String getCardsDetails(){
-        return "Here are the Cards details from my DB";
+    public List<Cards> getCardDetails(@RequestParam int id) {
+        List<Cards> cards = cardsRepository.findByCustomerId(id);
+        if (cards != null ) {
+            return cards;
+        }else {
+            return null;
+        }
     }
+
 }
