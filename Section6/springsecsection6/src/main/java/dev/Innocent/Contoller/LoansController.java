0a27042192cba2f446c48164350fe21d4bcd10aa
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2024-01-01 10:05:59 +0100

    CORS & CSRF

--- Section6/springsecsection6/src/main/java/dev/Innocent/Contoller/LoansController.java
+++ Section6/springsecsection6/src/main/java/dev/Innocent/Contoller/LoansController.java
@@ -1,13 +1,28 @@
 package dev.Innocent.Contoller;
 
+import dev.Innocent.Model.Loans;
+import dev.Innocent.Repository.LoanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class LoansController {
 
+    @Autowired
+    private LoanRepository loanRepository;
+
     @GetMapping("/myLoans")
-    public String getLoansDetails(){
-        return "Here are the loans details from my DB";
+    public List<Loans> getLoanDetails(@RequestParam int id) {
+        List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(id);
+        if (loans != null ) {
+            return loans;
+        }else {
+            return null;
+        }
     }
+
 }
