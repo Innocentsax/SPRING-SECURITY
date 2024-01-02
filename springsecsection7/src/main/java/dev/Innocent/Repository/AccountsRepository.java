319447c074130471b29c2476e2b574f46d68ff8a
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2024-01-02 19:35:31 +0100

    Implementing Authorization

---
+++ springsecsection7/src/main/java/dev/Innocent/Repository/AccountsRepository.java
@@ -0,0 +1,12 @@
+package dev.Innocent.Repository;
+
+import dev.Innocent.Model.Accounts;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends CrudRepository<Accounts, Long> {
+
+    Accounts findByCustomerId(int customerId);
+
+}
