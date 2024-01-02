8da531f3353af44cd53cc1e5d1db4662c693938c
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2024-01-02 18:21:50 +0100

    CORS & CSRF

--- Section6/00_UI_Initial Code/bank-app-ui/src/app/components/login/login.component.ts
+++ Section6/00_UI_Initial Code/bank-app-ui/src/app/components/login/login.component.ts
@@ -1,36 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { User } from "src/app/model/user.model";
-import { NgForm } from '@angular/forms';
-import { LoginService } from 'src/app/services/login/login.service';
-import { Router } from '@angular/router';
-
+import { NgForm } from "@angular/forms";
+import { LoginService } from "src/app/services/login/login.service";
+import { Router } from "@angular/router";
+import { getCookie } from "typescript-cookie";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
   authStatus: string = "";
   model = new User();
 
-  constructor(private loginService: LoginService, private router: Router) {
+  constructor(private loginService: LoginService, private router: Router) {}
 
-   }
-
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   validateUser(loginForm: NgForm) {
-    this.loginService.validateLoginDetails(this.model).subscribe(
-      responseData => {
-        this.model = <any> responseData.body;
-        this.model.authStatus = 'AUTH';
-        window.sessionStorage.setItem("userdetails",JSON.stringify(this.model));
-        this.router.navigate(['dashboard']);
+    this.loginService
+      .validateLoginDetails(this.model)
+      .subscribe((responseData) => {
+        this.model = <any>responseData.body;
+        let xsrf = getCookie("XSRF-TOKEN")!;
+        window.sessionStorage.setItem("XSRF-TOKEN", xsrf);
+        this.model.authStatus = "AUTH";
+        window.sessionStorage.setItem(
+          "userdetails",
+          JSON.stringify(this.model)
+        );
+        this.router.navigate(["dashboard"]);
       });
-
   }
-
 }
