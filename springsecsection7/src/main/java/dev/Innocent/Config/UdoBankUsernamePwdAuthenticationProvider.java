3f18f53924aaa0c48e655b68ab316a680a3a0017
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2024-01-02 20:43:32 +0100

    Implementing Authorization

--- springsecsection7/src/main/java/dev/Innocent/Config/UdoBankUsernamePwdAuthenticationProvider.java
+++ springsecsection7/src/main/java/dev/Innocent/Config/UdoBankUsernamePwdAuthenticationProvider.java
@@ -1,5 +1,6 @@
 package dev.Innocent.Config;
 
+import dev.Innocent.Model.Authority;
 import dev.Innocent.Model.Customer;
 import dev.Innocent.Repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Component
 public class UdoBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
@@ -35,15 +37,22 @@ public class UdoBankUsernamePwdAuthenticationProvider implements AuthenticationP
         List<Customer> customer = customerRepository.findByEmail(username);
         if(!customer.isEmpty()){
             if(passwordEncoder.matches(pwd, customer.get(0).getPwd())){
-                List<GrantedAuthority> authorities = new ArrayList<>();
-                authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
-                return new UsernamePasswordAuthenticationToken(username, pwd, authorities);
+                return new UsernamePasswordAuthenticationToken(username, pwd,
+                        getGrantedAuthorities(customer.get(0).getAuthorities()));
             }else{
                 throw new BadCredentialsException("Invalid password!");
             }
         }else{
             throw new BadCredentialsException("No user registered with this details");
         }
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(Set<Authority> authorities){
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        for(Authority authority: authorities){
+            grantedAuthorities.add(new SimpleGrantedAuthority(authority.getName()));
+        }
+        return grantedAuthorities;
     }
 
     @Override
