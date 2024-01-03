4daebcf5b44f429b3d37a00ea6e30c91b7b53524
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-03 17:30:18 +0100

    Designing Custom filters

---
+++ springsecsection8/src/main/java/dev/Innocent/Constants/ApplicationConstants.java
@@ -0,0 +1,8 @@
+package dev.Innocent.Constants;
+
+public class ApplicationConstants {
+
+    public static final String ERROR_MSG = "An unknown error occurred";
+    public static final String SUCCESS_MSG = "Request Successfully processed";
+
+}
---
+++ springsecsection8/src/main/java/dev/Innocent/Repository/CardsRepository.java
@@ -0,0 +1,14 @@
+package dev.Innocent.Repository;
+
+import dev.Innocent.Model.Cards;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CardsRepository extends CrudRepository<Cards, Long> {
+
+    List<Cards> findByCustomerId(int customerId);
+
+}
