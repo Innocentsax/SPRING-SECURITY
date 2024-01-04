0e9582af27b77f794263307a26539bf85e39651e
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-04 17:49:55 +0100

    JWT token

---
+++ springsecsection9/src/main/java/dev/Innocent/Constants/SecurityConstants.java
@@ -0,0 +1,6 @@
+package dev.Innocent.Constants;
+
+public class SecurityConstants {
+    public static final String JWT_KEY = "jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4";
+    public static final String JWT_HEADER = "Authorization";
+}
