0aad1235773b04e7370a1dd0f89756024b3d66f1
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-04 19:17:04 +0100

    JWT token

--- Section6/00_UI_Initial Code/bank-app-ui/src/app/components/login/login.component.ts
+++ Section6/00_UI_Initial Code/bank-app-ui/src/app/components/login/login.component.ts
@@ -22,6 +22,7 @@ export class LoginComponent implements OnInit {
     this.loginService
       .validateLoginDetails(this.model)
       .subscribe((responseData) => {
+        window.sessionStorage.setItem("Authorization", responseData.headers.get("Authorization")!);
         this.model = <any>responseData.body;
         let xsrf = getCookie("XSRF-TOKEN")!;
         window.sessionStorage.setItem("XSRF-TOKEN", xsrf);
