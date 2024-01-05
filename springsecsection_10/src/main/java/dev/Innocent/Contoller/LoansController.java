6a6a959b8933943845a47bccf461b4124cb3fb43
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-05 07:50:36 +0100

    Method Level Security

--- springsecsection_10/src/main/java/dev/Innocent/Contoller/LoansController.java
+++ springsecsection_10/src/main/java/dev/Innocent/Contoller/LoansController.java
@@ -3,6 +3,7 @@ package dev.Innocent.Contoller;
 import dev.Innocent.Model.Loans;
 import dev.Innocent.Repository.LoanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class LoansController {
     private LoanRepository loanRepository;
 
     @GetMapping("/myLoans")
+    @PostAuthorize("hasRole('USER')")
     public List<Loans> getLoanDetails(@RequestParam int id) {
         List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(id);
         if (loans != null ) {
