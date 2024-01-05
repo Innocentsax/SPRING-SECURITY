8ea23f2e86237497e769c8b7a9873965f1ad5090
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-05 08:35:52 +0100

    Method Level Security

--- springsecsection_10/src/main/java/dev/Innocent/Contoller/ContactController.java
+++ springsecsection_10/src/main/java/dev/Innocent/Contoller/ContactController.java
@@ -3,11 +3,14 @@ package dev.Innocent.Contoller;
 import dev.Innocent.Model.Contact;
 import dev.Innocent.Repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @RestController
@@ -16,11 +19,23 @@ public class ContactController {
     @Autowired
     private ContactRepository contactRepository;
 
+//    @PostMapping("/contact")
+//    public Contact saveContactInquiryDetails(@RequestBody Contact contact) {
+//        contact.setContactId(getServiceReqNumber());
+//        contact.setCreateDt(new Date(System.currentTimeMillis()));
+//        return contactRepository.save(contact);
+//    }
+
     @PostMapping("/contact")
-    public Contact saveContactInquiryDetails(@RequestBody Contact contact) {
+    @PreFilter("filterObject.contactName != 'Test'")
+    public List<Contact> saveContactInquiryDetails(@RequestBody List<Contact> contacts){
+        Contact contact = contacts.get(0);
         contact.setContactId(getServiceReqNumber());
         contact.setCreateDt(new Date(System.currentTimeMillis()));
-        return contactRepository.save(contact);
+        contact = contactRepository.save(contact);
+        List<Contact> returnContacts = new ArrayList<>();
+        returnContacts.add(contact);
+        return returnContacts;
     }
 
     public String getServiceReqNumber() {
