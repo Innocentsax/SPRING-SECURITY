a1b766c7ea5910f941695365989ef6ee7394e839
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-05 07:50:31 +0100

    Method Level Security

--- springsecsection_10/src/main/java/dev/Innocent/Repository/LoanRepository.java
+++ springsecsection_10/src/main/java/dev/Innocent/Repository/LoanRepository.java
@@ -2,7 +2,6 @@ package dev.Innocent.Repository;
 
 import dev.Innocent.Model.Loans;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +9,7 @@ import java.util.List;
 @Repository
 public interface LoanRepository extends CrudRepository<Loans, Long> {
 
-    @PreAuthorize("hasRole('USER')")
+    //@PreAuthorize("hasRole('USER')")
     List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
 
 }
