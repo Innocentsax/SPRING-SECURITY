584a285897905859c945c4f14f24bd3c608569a0
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-05 15:30:15 +0100

    Implementing OAUTH2 using Spring Security

---
+++ springsecOAUTH2Github/src/main/java/dev/Innocent/SpringsecOauth2GithubApplication.java
@@ -0,0 +1,13 @@
+package dev.Innocent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecOauth2GithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecOauth2GithubApplication.class, args);
+	}
+
+}
