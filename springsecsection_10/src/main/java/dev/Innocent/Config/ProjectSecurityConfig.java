3ba73899baefbd35506aac39718e625b72b62bb0
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-05 06:26:39 +0100

    Method Level Security

--- springsecsection_10/src/main/java/dev/Innocent/Config/ProjectSecurityConfig.java
+++ springsecsection_10/src/main/java/dev/Innocent/Config/ProjectSecurityConfig.java
@@ -51,7 +51,7 @@ public class ProjectSecurityConfig {
                 .authorizeHttpRequests((requests)->requests
                         .requestMatchers("/myAccount").hasRole("USER")
                         .requestMatchers("/myBalance").hasAnyRole("USER","ADMIN")
-                        .requestMatchers("/myLoans").hasRole("USER")
+                        .requestMatchers("/myLoans").authenticated()
                         .requestMatchers("/myCards").hasRole("USER")
                         .requestMatchers("/user").authenticated()
                         .requestMatchers("/notices","/contact","/register").permitAll())
