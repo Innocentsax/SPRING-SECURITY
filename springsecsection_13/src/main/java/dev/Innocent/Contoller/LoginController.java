d7e08a288e503f0e7ceaa2981ef2e127e9e6cbac
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-06 14:51:08 +0100

    OAUTH2 Style Login with Spring Boot

--- springsecsection_13/src/main/java/dev/Innocent/Contoller/LoginController.java
+++ springsecsection_13/src/main/java/dev/Innocent/Contoller/LoginController.java
@@ -3,21 +3,14 @@ package dev.Innocent.Contoller;
 import dev.Innocent.Model.Customer;
 import dev.Innocent.Repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.Date;
 import java.util.List;
-
 @RestController
 public class LoginController {
-
+/*
     @Autowired
     private CustomerRepository customerRepository;
 
@@ -50,6 +43,20 @@ public class LoginController {
     public Customer getUserDetailsAfterLogin(Authentication authentication) {
         List<Customer> customers = customerRepository.findByEmail(authentication.getName());
         if (!customers.isEmpty()) {
+            return customers.get(0);
+        } else {
+            return null;
+        }
+
+    } */
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @RequestMapping("/user")
+    public Customer getUserDetailsAfterLogin(Authentication authentication) {
+        List<Customer> customers = customerRepository.findByEmail(authentication.getName());
+        if (customers.size() > 0) {
             return customers.get(0);
         } else {
             return null;
