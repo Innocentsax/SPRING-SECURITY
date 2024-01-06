b0b57620dd033f1b8dcd37bd0b88500d41f1ce1d
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-06 14:51:03 +0100

    OAUTH2 Style Login with Spring Boot

--- springsecsection_13/src/main/java/dev/Innocent/Contoller/LoansController.java
+++ springsecsection_13/src/main/java/dev/Innocent/Contoller/LoansController.java
@@ -1,9 +1,10 @@
 package dev.Innocent.Contoller;
 
+import dev.Innocent.Model.Customer;
 import dev.Innocent.Model.Loans;
+import dev.Innocent.Repository.CustomerRepository;
 import dev.Innocent.Repository.LoanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,15 +17,19 @@ public class LoansController {
     @Autowired
     private LoanRepository loanRepository;
 
+    @Autowired
+    private CustomerRepository customerRepository;
+
     @GetMapping("/myLoans")
-    @PostAuthorize("hasRole('USER')")
-    public List<Loans> getLoanDetails(@RequestParam int id) {
-        List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(id);
-        if (loans != null ) {
-            return loans;
-        }else {
-            return null;
+    public List<Loans> getLoanDetails(@RequestParam String email) {
+        List<Customer> customers = customerRepository.findByEmail(email);
+        if (customers != null && !customers.isEmpty()) {
+            List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(customers.get(0).getId());
+            if (loans != null ) {
+                return loans;
+            }
         }
+        return null;
     }
 
 }
