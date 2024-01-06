0afb3701294dc9cf1fab4a8fda71e63f81fbd90c
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-06 14:51:19 +0100

    OAUTH2 Style Login with Spring Boot

--- springsecsection_13/src/main/java/dev/Innocent/Constants/SecurityConstants.java
+++
@@ -1,6 +0,0 @@
-package dev.Innocent.Constants;
-
-public class SecurityConstants {
-    public static final String JWT_KEY = "jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4";
-    public static final String JWT_HEADER = "Authorization";
-}
