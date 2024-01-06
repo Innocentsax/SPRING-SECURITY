7fcb37f3f1e7902f1235ab2bcfe3ad08536b4299
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-06 14:50:48 +0100

    OAUTH2 Style Login with Spring Boot

--- springsecsection_13/src/main/java/dev/Innocent/Contoller/ContactController.java
+++ springsecsection_13/src/main/java/dev/Innocent/Contoller/ContactController.java
@@ -15,26 +15,18 @@ import java.util.Random;
 
 @RestController
 public class ContactController {
-
     @Autowired
     private ContactRepository contactRepository;
 
-//    @PostMapping("/contact")
-//    public Contact saveContactInquiryDetails(@RequestBody Contact contact) {
-//        contact.setContactId(getServiceReqNumber());
-//        contact.setCreateDt(new Date(System.currentTimeMillis()));
-//        return contactRepository.save(contact);
-//    }
-
     @PostMapping("/contact")
-    //@PreFilter("filterObject.contactName != 'Test'")
+    /*@PreFilter("filterObject.contactName != 'Test'")*/
     @PostFilter("filterObject.contactName != 'Test'")
-    public List<Contact> saveContactInquiryDetails(@RequestBody List<Contact> contacts){
+    public List<Contact> saveContactInquiryDetails(@RequestBody List<Contact> contacts) {
         Contact contact = contacts.get(0);
         contact.setContactId(getServiceReqNumber());
         contact.setCreateDt(new Date(System.currentTimeMillis()));
         contact = contactRepository.save(contact);
-        List<Contact> returnContacts = new ArrayList<>();
+        List<Contact> returnContacts = new ArrayList();
         returnContacts.add(contact);
         return returnContacts;
     }
