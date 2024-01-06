947ce04a735eada4a731d043bb22d2540c285e45
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-06 14:50:18 +0100

    OAUTH2 Style Login with Spring Boot

--- springsecsection_13/src/main/java/dev/Innocent/Contoller/AccountController.java
+++ springsecsection_13/src/main/java/dev/Innocent/Contoller/AccountController.java
@@ -1,26 +1,33 @@
 package dev.Innocent.Contoller;
 
 import dev.Innocent.Model.Accounts;
+import dev.Innocent.Model.Customer;
 import dev.Innocent.Repository.AccountsRepository;
+import dev.Innocent.Repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class AccountController {
-
     @Autowired
     private AccountsRepository accountsRepository;
 
-    @GetMapping("/myAccount")
-    public Accounts getAccountDetails(@RequestParam int id) {
-        Accounts accounts = accountsRepository.findByCustomerId(id);
-        if (accounts != null ) {
-            return accounts;
-        }else {
-            return null;
-        }
-    }
+    @Autowired
+    private CustomerRepository customerRepository;
 
+    @GetMapping("/myAccount")
+    public Accounts getAccountDetails(@RequestParam String email) {
+        List<Customer> customers = customerRepository.findByEmail(email);
+        if (customers != null && !customers.isEmpty()) {
+            Accounts accounts = accountsRepository.findByCustomerId(customers.get(0).getId());
+            if (accounts != null) {
+                return accounts;
+            }
+        }
+        return null;
+    }
 }
