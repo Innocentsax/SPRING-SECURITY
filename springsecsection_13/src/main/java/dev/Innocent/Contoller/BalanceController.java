61ec07c3e132e1793e419d3f05c2d47677349e8a
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-06 14:50:36 +0100

    OAUTH2 Style Login with Spring Boot

--- springsecsection_13/src/main/java/dev/Innocent/Contoller/BalanceController.java
+++ springsecsection_13/src/main/java/dev/Innocent/Contoller/BalanceController.java
@@ -1,7 +1,9 @@
 package dev.Innocent.Contoller;
 
 import dev.Innocent.Model.AccountTransactions;
+import dev.Innocent.Model.Customer;
 import dev.Innocent.Repository.AccountTransactionsRepository;
+import dev.Innocent.Repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,16 +15,20 @@ import java.util.List;
 public class BalanceController {
 
     @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
     private AccountTransactionsRepository accountTransactionsRepository;
 
     @GetMapping("/myBalance")
-    public List<AccountTransactions> getBalanceDetails(@RequestParam int id) {
-        List<AccountTransactions> accountTransactions = accountTransactionsRepository.
-                findByCustomerIdOrderByTransactionDtDesc(id);
-        if (accountTransactions != null ) {
-            return accountTransactions;
-        }else {
-            return null;
+    public List<AccountTransactions> getBalanceDetails(@RequestParam String email) {
+        List<Customer> customers = customerRepository.findByEmail(email);
+        if (customers != null && !customers.isEmpty()) {
+            List<AccountTransactions> accountTransactions = accountTransactionsRepository.
+                    findByCustomerIdOrderByTransactionDtDesc(customers.get(0).getId());
+            if (accountTransactions != null ) {
+                return accountTransactions;
+            }
         }
+        return null;
     }
 }
